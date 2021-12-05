01cd003cc88a33320065c3a3953e6dbee2aba5f4
Nurbolat <turgynbekov.nurbolat.nurlanovich@gmail.com>
2021-12-05 14:38:14 +0600

    added-some

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -2,35 +2,13 @@ import React, { lazy } from 'react';
 import { Route, Routes } from 'react-router';
 import { MainLayout } from '@/containers';
 import { Loadable } from '@/components';
-import DefaultMain from '../views/main';
-import DefaultMain1 from '../views/main1';
-import DefaultMain2 from '../views/main2';
 
 
-// const DefaultMain = Loadable(lazy(() => import(/* @/views/main */ '@/views/main')));
+const DefaultMain = Loadable(lazy(() => import(/* @/views/main */ '@/views/main')));
 
-// const DefaultMain1 = Loadable(lazy(() => import(/* @/views/main1 */ '@/views/main1')));
+const DefaultMain1 = Loadable(lazy(() => import(/* @/views/main1 */ '@/views/main1')));
 
-// const DefaultMain2 = Loadable(lazy(() => import(/* @/views/main2 */ '@/views/main2')));
-
-// const MainRoutes = {
-//     path: '/',
-//     element: <MainLayout />,
-//     children: [
-//         {
-//             path: '/',
-//             element: <DefaultMain />
-//         },
-//         {
-//             path: '/dashboard/default',
-//             element: <DefaultMain1 />
-//         },
-//         {
-//             path: '/*',
-//             element: <DefaultMain2 />
-//         }
-//     ]
-// };
+const DefaultMain2 = Loadable(lazy(() => import(/* @/views/main2 */ '@/views/main2')));
 
 const MainRoutes = () => (
     <Routes>
